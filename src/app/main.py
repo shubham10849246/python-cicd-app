c1fe44a44d285898d8725ecf59376922907c0fda
Shubham <Shubham@gmail.com>
2026-04-01 09:00:29 +0000

    Changes

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .routes import router
 
-app = FastAPI(title="Python CI/CD Demo App", version="1.0.0")
+app = FastAPI(title="Python CI/CD Demo App for Shubham", version="1.0.0")
 
 @app.get("/health")
 def health():
